4c02c11190afc21c184d6d1cec36419fde44726e
Anant raj <heyanantraj@gmail.com>
2022-06-27 02:57:14 -0500

    added initial data models and registered on the admin UI

--- sbProto/post/models.py
+++ sbProto/post/models.py
@@ -1,3 +1,14 @@
-from django.db import models
-
+from django.db import migrations, models
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    TimePublished = models.DateTimeField()
+    image = models.ImageField(upload_to='files/images', default='default.png')
+
+    def __str__(self):
+        return self.title + " / " + str(self.author) + " / " + str(self.TimePublished)
